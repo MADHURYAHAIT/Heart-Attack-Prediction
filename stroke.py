d651652f7e99041e4785a1cc78de10326456e365
Madhurya Hait <madhurya_202100516@smit.smu.edu.in>
2023-12-31 00:39:33 +0530

    model changed

--- stroke.py
+++ stroke.py
@@ -1,8 +1,12 @@
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score , confusion_matrix
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 import pandas as pd
 import numpy as np
+
+from imblearn.over_sampling import SMOTE
+from xgboost import XGBClassifier
 
 df= pd.read_csv('healthcare-dataset-stroke-data.csv')
 df.drop(['id'], axis=1, inplace=True)
@@ -29,19 +33,42 @@ features = ["age",'bmi','hypertension', 'heart_disease', 'ever_married', 'work_t
 
 X = df[features]
 Y = df['stroke']
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2 , random_state=48)
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3 , random_state=1255)
+train_x,test_x,train_y,test_y=X_train, X_test, Y_train, Y_test 
+# smote=SMOTE()
+# train_x,train_y=smote.fit_resample(X_train,Y_train)
+# test_x,test_y=smote.fit_resample(X_test,Y_test)
+
+xgc=XGBClassifier(objective='binary:logistic',n_estimators=100000,max_depth=5,learning_rate=0.001,n_jobs=-1)
+xgc.fit(train_x,train_y)
+Y_pred=xgc.predict(test_x)
+print(Y_pred.tolist())
+accuracy = accuracy_score(test_y, Y_pred)
+confusion = confusion_matrix(Y_test, Y_pred)
+print("Accuracy --> ",accuracy)
+print("Confusion Matrix:\n", confusion)
+
 
 #train 
 
-rf_classifier = RandomForestClassifier(n_estimators=100, random_state=42)
+# rf_classifier = RandomForestClassifier(n_estimators=100, random_state=42)
+# model = LogisticRegression()
 
-rf_classifier.fit(X_train, Y_train)
+# rf_classifier.fit(X_train, Y_train)
+# model.fit(X_train, Y_train)
 
-Y_pred = rf_classifier.predict(X_test)
+# Y_pred = rf_classifier.predict(X_test)
+# y_pred = model.predict(X_test)
 
-accuracy = accuracy_score(Y_test, Y_pred)
+# accuracy = accuracy_score(Y_test, y_pred)
+# confusion = confusion_matrix(Y_test, y_pred)
 
-print("Accuracy:", accuracy)
+# print(Y_test.tolist())
+# print(Y_pred.tolist())
+
+# print("Accuracy:", accuracy)
+# print("Confusion Matrix:\n", confusion)
 
 print("================================== Enter the following details to check if you are likely to have a stroke ==================================\n")
 
@@ -56,7 +83,7 @@ Residence_type=int(input("Enter Residence_type: Rural': 0, 'Urban': 1 : "))
 smoking_status=int(input("Enter smoking_status: smokes': 0, 'formerly smoked': 1, 'unknown': 2, 'never smoked' : 3 : "))
 gender=int(input("Enter Gender: 0 for Male and 1 for Female: "))
 
-k=rf_classifier.predict([[age,bmi,hypertension,heart_disease,ever_married,work_type,avg_glucose_level,Residence_type,smoking_status,gender]])
+k=xgc.predict([[age,bmi,hypertension,heart_disease,ever_married,work_type,avg_glucose_level,Residence_type,smoking_status,gender]])
 
 print("\n ==================================================== Result ==========================================================\n")
 if k==0:
